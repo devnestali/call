859ce21617c0ef2c84cc6896d60b4c527a40b87a
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-07-17 12:09:04 +0200

    feat(CalendarStep): Creating states on CalendarStep to open the TimePicker according with day selected on calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -22,7 +22,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[]
 
-export function Calendar() {
+interface CalendarProps {
+  selectedDate?: Date | null
+  onDateSelected: (date: Date) => void
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
@@ -76,7 +81,7 @@ export function Calendar() {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false }
+        return { date, disabled: date.endOf('day').isBefore(new Date()) }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -137,7 +142,10 @@ export function Calendar() {
                       date={date.format('DD [de] MMMM')}
                       disponibility={disabled ? 'Indisponíble' : 'Disponíble'}
                     >
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay
+                        disabled={disabled}
+                        onClick={() => onDateSelected(date.toDate())}
+                      >
                         {date.get('date')}
                       </CalendarDay>
                     </Tooltip>
